8e371c5edbcf6c6fc40bea61d93f3ad852e5fc06
Douglas de Souza Silva <douglassouzasilva888@gmail.com>
2021-03-05 03:20:57 -0300

    Primeiro commit
    Criando as properties da classe Reservation

---
+++ HospedeHotel/HospedeHotel/Entities/Reservation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospedeHotel.Entities {
+    class Reservation {
+        public int roomNumber { get; set; }
+    }
+}
